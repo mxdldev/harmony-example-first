b9b75c45e2628f04f96ce0e02784652d8d2c34ef
guoxl <guoxl@pep.com.cn>
2024-01-31 17:25:58 +0800

    secondd commit

--- entry/src/main/ets/entryability/SecondAbility.ts
+++ entry/src/main/ets/entryability/SecondAbility.ts
@@ -1,9 +1,12 @@
 import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
+import window from '@ohos.window';
 
 export class SecondAbility extends UIAbility {
-  onWindowStageCreate(window) {
-    // @ts-ignore
+  onWindowStageCreate(windowStage: window.WindowStage) {
+    // Main window is created, set main page for this ability
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
+
     windowStage.loadContent('pages/Second', (err, data) => {
       if (err.code) {
         hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
